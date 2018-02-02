a60a0fc47f68a23630f1fd157ea4633b90447624
LogalakshmiRajendran <35220631+LogalakshmiRajendran@users.noreply.github.com>
2018-02-02 16:19:36 +0530

    Create 34th.c

---
+++ 34th.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+int a,b=0,i;
+char s[1000];
+//puts(enter the lines);
+scanf(s);
+for(i=0;s[i]!=NULL;i++)
+{
+if(s[i]=='.')
+b++;
+}
+printf("%d",b);
+}
+
