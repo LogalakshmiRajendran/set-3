87de8ae72fef4c14324b8f2a963e6ffffe323884
LogalakshmiRajendran <35220631+LogalakshmiRajendran@users.noreply.github.com>
2018-02-05 16:44:40 +0530

    Create 44.c

---
+++ 44.c
@@ -0,0 +1,15 @@
+# include<stdio.h>
+void main()
+{
+    int a;
+    printf("enter num");
+    scanf("%d",&a);
+    if((a>=1)&&(a<=10))
+    {
+    printf("range");
+    }
+    else
+    {
+        printf("not in range");
+    }
+}
