57b6b531b16c2f6780d0c2bff15b1ae3ccfbdd35
LogalakshmiRajendran <35220631+LogalakshmiRajendran@users.noreply.github.com>
2018-02-05 15:41:26 +0530

    Update 35th.c

--- 35th.c
+++ 35th.c
@@ -1,1 +1,18 @@
 #include<stdio.h>
+#include<string.h.
+int main()
+{
+char str[200];
+int i,count;
+printf("\n Enter the string");
+gets("%s",&str);
+for(i=0;str[i]!='\0';i++)
+{
+if(str[i]>='0'&&str[i]<='9')
+{
+count++;
+}
+}
+printf("\n The numeric value is %d",count);
+return o;
+}
