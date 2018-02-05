a8a4ce4a4a5ea3267eb6e2d82148999812ac5a3e
LogalakshmiRajendran <35220631+LogalakshmiRajendran@users.noreply.github.com>
2018-02-05 16:00:21 +0530

    Create 37.c

---
+++ 37.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int a,b;
+printf("enter two numbers");
+scanf("%d%d",&a,&b);
+printf("\nbefore swap %d %d",a,b);
+a=a+b;
+b=a-b;
+a=a-b;
+printf("\nafter swap %d %d",a,b);
+return 0;
+}
